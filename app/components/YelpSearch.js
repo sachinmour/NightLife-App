bbbbe5c1bca8cd5d6594c729ce988a2c19805b45
Sachin Mour <rtr.sachinmour@gmail.com>
2016-04-29 16:25:40 +0000

    YelpSearch.js

---
+++ app/components/YelpSearch.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Router from 'react-router';
+
+class SearchYelp extends React.Component{
+  
+  getRef(ref) {
+    this.usernameRef = ref;
+  }
+  
+  handleSubmit() {
+    var username = this.usernameRef.value;
+    this.usernameRef.value = '';
+    this.props.history.push("/profile/"+username);
+  }
+  render() {
+    return (
+      <div className="col-sm-12">
+        <form onSubmit={() => this.handleSubmit()}>
+          <div className="form-group col-sm-7">
+            <input type="text" className="form-control" ref={(ref) => this.getRef(ref)} />
+          </div>
+          <div className='form=group col-sm-5'>
+            <button type='submit' className='btn btn-block btn-primary'>Search Github</button>
+          </div>
+        </form>
+      </div>  
+    );
+  }
+}
+
+SearchGithub.PropTypes = {
+  history: React.PropTypes.object.isRequired
+};
+
+export default SearchGithub;
