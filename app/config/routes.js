17509b13cf7ad784cfceeaf481771521693892fe
Sachin Mour <rtr.sachinmour@gmail.com>
2016-05-02 18:31:16 +0000

    updating location/starting to use yelp api

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Main from "../components/Main";
 import Home from "../components/Home";
+import Location from "../components/Location";
 
 import {Route, IndexRoute} from 'react-router';
 
 export default (
   <Route path='/' component={Main}>
+    <Route path="place/:username" component={Location} />
     <IndexRoute component={Home} />
   </Route>
 );
