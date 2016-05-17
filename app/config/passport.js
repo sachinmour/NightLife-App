13ee6d6ab38bd08d06313d2237c9ad41fc09eecf
Sachin Mour <rtr.sachinmour@gmail.com>
2016-05-17 20:55:39 +0000

    Configuring Passport

---
+++ app/config/passport.js
@@ -0,0 +1,155 @@
+ar GitHubStrategy = require('passport-github').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/users');
+var configAuth = require('./auth');
+
+module.exports = function (passport) {
+	passport.serializeUser(function (user, done) {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function (id, done) {
+		User.findById(id, function (err, user) {
+			done(err, user);
+		});
+	});
+	
+	passport.use(new GoogleStrategy({
+		clientID: configAuth.googleAuth.clientID,
+		clientSecret: configAuth.googleAuth.clientSecret,
+		callbackURL: configAuth.googleAuth.callbackURL
+	},
+	function (token, refreshToken, profile, done) {
+		process.nextTick(function () {
+			User.findOne({ 'google.id': profile.id }, function (err, user) {
+				if (err) {
+					return done(err);
+				}
+
+				if (user) {
+					return done(null, user);
+				} else {
+					var newUser = new User();
+					newUser.google.id = profile.id;
+					newUser.google.displayName = profile.displayName;
+					
+					newUser.save(function (err) {
+						if (err) {
+							throw err;
+						}
+
+						return done(null, newUser);
+					});
+				}
+			});
+		});
+	}));
+	
+	passport.use(new FacebookStrategy({
+		clientID: configAuth.facebookAuth.clientID,
+		clientSecret: configAuth.facebookAuth.clientSecret,
+		callbackURL: configAuth.facebookAuth.callbackURL
+	},
+	function (token, refreshToken, profile, done) {
+		process.nextTick(function () {
+			User.findOne({ 'facebook.id': profile.id }, function (err, user) {
+				if (err) {
+					return done(err);
+				}
+
+				if (user) {
+					return done(null, user);
+				} else {
+					var newUser = new User();
+					
+					newUser.facebook.id = profile.id;
+					newUser.facebook.displayName = profile.displayName;
+					
+					newUser.save(function (err) {
+						if (err) {
+							throw err;
+						}
+
+						return done(null, newUser);
+					});
+				}
+			});
+		});
+	}));
+
+	passport.use(new GitHubStrategy({
+		clientID: configAuth.githubAuth.clientID,
+		clientSecret: configAuth.githubAuth.clientSecret,
+		callbackURL: configAuth.githubAuth.callbackURL
+	},
+	function (token, refreshToken, profile, done) {
+		process.nextTick(function () {
+			User.findOne({ 'github.id': profile.id }, function (err, user) {
+				if (err) {
+					return done(err);
+				}
+
+				if (user) {
+					return done(null, user);
+				} else {
+					var newUser = new User();
+
+					newUser.github.id = profile.id;
+					newUser.github.username = profile.username;
+					newUser.github.displayName = profile.displayName;
+					newUser.github.publicRepos = profile._json.public_repos;
+
+					newUser.save(function (err) {
+						if (err) {
+							throw err;
+						}
+
+						return done(null, newUser);
+					});
+				}
+			});
+		});
+	}));
+	
+	
+/**
+ * Sign in using Email and Password.
+//  */
+// passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+//   email = email.toLowerCase();
+//   User.findOne({ email: email }, function(err, user) {
+//     if (!user) {
+//       return done(null, false, { message: 'Email ' + email + ' not found'});
+//     }
+//     user.comparePassword(password, function(err, isMatch) {
+//       if (isMatch) {
+//         return done(null, user);
+//       } else {
+//         return done(null, false, { message: 'Invalid email or password.' });
+//       }
+//     });
+//   });
+// }));
+
+passport.use(new LocalStrategy({ usernameField: 'email' },
+  function(email, password, done) {
+    User.findOne({ email: email }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Email not found' });
+      }
+      user.comparePassword(password, function(err, isMatch) {
+	      if (isMatch) {
+		        return done(null, user);
+		      } else {
+		        return done(null, false, { message: 'Invalid password.' });
+		      }
+	  });
+    });
+  }
+));
+	
+	
+};
